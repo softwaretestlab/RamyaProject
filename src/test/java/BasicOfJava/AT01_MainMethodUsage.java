7eb3aaebe4c3137c1763f34e069ba54a791baea2
softwaretestlab <softwaretestlab@hotmail.com>
2022-08-10 09:48:50 +1000

    Basics

---
+++ src/test/java/BasicOfJava/AT01_MainMethodUsage.java
@@ -0,0 +1,12 @@
+//this is a package
+package BasicOfJava;
+//this is a class
+public class AT01_MainMethodUsage {
+    //this is main method
+    public static void main(String[] args){
+        //this is where we write actual code
+        System.out.println(" This is my First Java class");
+
+    }
+
+}
---
+++ src/test/java/BasicOfJava/AT02_LearnAboutMethod_JUnit.java
@@ -0,0 +1,21 @@
+package BasicOfJava;
+
+import org.junit.Test;
+
+public class AT02_LearnAboutMethod_JUnit {
+    //here test01 is the method name
+    //here @Test is junit annotation
+    @Test
+    public void test01(){
+        //code here
+        System.out.println("This is method 1");
+    }
+
+    @Test
+    public void test02(){
+        //code here
+        System.out.println("This is method 2");
+    }
+
+
+}
