ee5d752a054abeb3973513c402d9941c29570e32
softwaretestlab <softwaretestlab@hotmail.com>
2022-08-19 09:58:49 +1000

    NewChangesss

---
+++ src/test/java/SeleniumLearning/AT02_LocatorsLearning.java
@@ -0,0 +1,45 @@
+package SeleniumLearning;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class AT02_LocatorsLearning {
+
+    @Test
+    public void locLearn() throws InterruptedException {
+        //Setting up chromedriver.
+        WebDriverManager.chromedriver().setup();
+        //options
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //we create an object of WebDriver
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        //At this point browser will be opened
+        //maximize browser
+        driver.manage().window().maximize();
+        // navigate to HN website
+        driver.get("https://www.thegoodguys.com.au/UserRegistrationForm?myAcctMain=1&new=Y&catalogId=30000&langId=-1&storeId=900");
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//by id
+        driver.findElement(By.id("regAddForm_firstName")).sendKeys("test01");
+        Thread.sleep(5000);
+        //by name
+        driver.findElement(By.name("lastName")).sendKeys("test02");
+        Thread.sleep(10000);
+        //by linktext
+      //  driver.findElement(By.linkText("Catalogues")).click();
+        //By xpath
+        driver.findElement(By.xpath("//*[@id='regAddForm_email1']")).sendKeys("abc@gmail.com");
+        //By Css Selector
+        driver.findElement(By.cssSelector("#regAddForm_mobileNum")).sendKeys("04389898899");
+    }
+}
