5f272b6f9e3c4e6425f6e7122d348c085550f8a8
softwaretestlab <softwaretestlab@hotmail.com>
2022-08-24 13:08:37 +1000

    changes

--- src/test/java/SeleniumLearning/AT04_iframeConcept.java
+++ src/test/java/SeleniumLearning/AT04_iframeConcept.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-public class AT04_mapinput {
+public class AT04_iframeConcept {
     @Test
     public void mpalearn() throws InterruptedException {
         //Setting up chromedriver.
