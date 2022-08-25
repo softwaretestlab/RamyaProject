8364d3f8d0c83fef3de58161ce8dc8fc48796956
softwaretestlab <softwaretestlab@hotmail.com>
2022-08-25 12:59:59 +1000

    changesss

---
+++ src/test/java/SeleniumLearning/AT05_NavigateBackFront.java
@@ -0,0 +1,37 @@
+package SeleniumLearning;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+    public class AT05_NavigateBackFront {
+        @Test
+        public void NavHn() throws InterruptedException, IOException {
+            //setting up chromedriver
+            WebDriverManager.chromedriver().setup();
+            //options
+            ChromeOptions chromeOptions = new ChromeOptions();
+            //we create an object of WebDriver
+            WebDriver driver = new ChromeDriver(chromeOptions);
+            //At this point browser will be opened
+            //maximize browser
+            driver.manage().window().maximize();
+            //Navigate to HN website
+            driver.get("https://echoecho.com/");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            //click on tutorials link on homepage
+            driver.findElement(By.xpath("/html/body/div[2]/table[7]/tbody/tr/td[4]/table/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/big/a")).click();
+            Thread.sleep(10000);
+            driver.navigate().back();
+            Thread.sleep(10000);
+            driver.navigate().forward();
+
+        }
+}
